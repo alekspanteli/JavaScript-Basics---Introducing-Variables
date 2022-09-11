e5ffdc12e16f334f8419711d5b4639f954fc0507
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-11 19:25:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-mvn77m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-mvn77m)
---
+++ let-const.js
@@ -0,0 +1,18 @@
+//Const value never changes
+// const score = 5;
+// score += 5;
+// console.log(score);
+
+
+// We can only reassign score value usings score
+// let score = 5;
+// score += 15;
+// score = 5;
+// console.log(score);
+
+
+// We can reassign score value using var again and varialbe name
+// var score = 5;
+// score += 15;
+// var score = 5;
+// console.log(score);
